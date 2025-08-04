61442b79689a44acccba6624a4be40f6c0ec10fd
kleide99 <alainekleidebaker@gmail.com>
2025-08-04 19:28:56 +0800

    Add files via upload

---
+++ library/library/models/library_book.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+from odoo import models,fields,api
+
+
+class LibraryBook(models.Model):
+    ######################
+    # Private attributes #
+    ######################
+    _name = "library.book"
+    _description = "Book"
+    _order = "name"
+
+    ###################
+    # Default methods #
+    ###################
+    name = fields.Char(string="Name",
+                       required=True)
+    isbn_13 = fields.Char(string="ISBN 13",
+                          required=True)
+    author_id = fields.Many2one(string="Author",
+                            required=True)
+    category_id = fields.Many2many(string="Category")
+
+
+    ######################
+    # Fields declaration #
+    ######################
+
+    ##############################
+    # Compute and search methods #
+    ##############################
+
+    ############################
+    # Constrains and onchanges #
+    ############################
+
+    #########################
+    # CRUD method overrides #
+    #########################
+
+    ##################
+    # Action methods #
+    ##################
+
+    ####################
+    # Business methods #
+    ####################
