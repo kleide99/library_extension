73edd5d18ff14a62478a56ce121430d3dbce9f29
kleide99 <alainekleidebaker@gmail.com>
2025-08-07 20:04:08 +0800

    Update library_book_category.py

--- V17test/addons/library_extensions/models/library_book_category.py
+++ V17test/addons/library_extensions/models/library_book_category.py
@@ -7,34 +7,40 @@ class LibraryBookCategory(models.Model):
     ######################
     # Private attributes #
     ######################
-    _name = "library.extension"
+    _name = "library.book.category"
     _description = "Book Category"
     _order = "name"
 
     ###################
     # Default methods #
     ###################
-    author_id = fields.Many2one(
-                            string="Name",
-                            comodel_name="res.partner",
-                            required=True,
-                            unique=True)
-    category_id = fields.Many2many(
-                            string="Category",
-                            required=True)
+    
 
     ######################
     # Fields declaration #
     ######################
+    author_id = fields.Many2one(
+                            string="Name",
+                            comodel_name="res.partner",
+                            required=True,)
+    
+    category_id = fields.Many2many(
+                            string="Category",
+                            required=True)
 
     ##############################
     # Compute and search methods #
     ##############################
 
     ############################
-    # Constrains and onchanges #
+    # Constraints and onchanges #
     ############################
 
+    _sql_constraints = [
+        ('unique_author_constraint', 'unique(author_id,), 'An author must be unique.'),]
+    _sql_constraints = [
+        ('unique_category_constraint', 'unique(category_id,), 'A category must be unique.'),]
+        
     #########################
     # CRUD method overrides #
     #########################
@@ -43,6 +49,15 @@ class LibraryBookCategory(models.Model):
     # Action methods #
     ##################
 
+    category_obj = env['library.book.category'].create({
+        'name': 'Action',}]
+    duplicate_category_obj = env['library.book.category'].create({
+        'name': 'Action',}]
+    author = env['res.partner'].create({
+        'name': 'Michael Jackson',}]
+    book = env['library.book']
+
     ####################
     # Business methods #
     ####################
+
